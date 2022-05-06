8f863e827d6791d9242365cf8519da25c2d3c720
james chandler <80052602+jameschandler1@users.noreply.github.com>
2022-05-06 16:29:32 -0700

    client

---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import React from "react";
+import ReactDOM from "react-dom";
+import Pages from "./pages";
+import GlobalStyles from "./styles";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <GlobalStyles />
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
